b5bb48511e026961649fa38df1d654977afe6f1e
pavikeerthana <35835887+pavikeerthana@users.noreply.github.com>
2018-01-30 17:00:23 +0530

    Rename power.c to power1.c

---
+++ power1.c
@@ -0,0 +1,17 @@
+#inclusde<coni.h>
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter the number");
+printf("ENTER THE EXPONENT);
+SCANF("%d",&a&b);
+while(c>0);
+{
+printf("enter the power number");
+}
+else
+{
+printf("enter the natural number");
+getch();
+}
